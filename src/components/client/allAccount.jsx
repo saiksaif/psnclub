88c13ad010fa49cf516fbc890876eeee35ce939b
saiksaif <116622210+saiksaif@users.noreply.github.com>
2023-07-05 15:06:53 +0500

    Added test data

--- src/components/client/allAccount.jsx
+++ src/components/client/allAccount.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from "react";
 
 const AllAccount = () => {
   return (
-    <div>
-      <h1>AllAccount</h1>
-    </div>
-  )
-}
+    <>
+        <nav>
+          <label  for="touch">
+            <span className="DropDownProductsData">
+              <p>Id</p>
+              <p>price</p>
+              <p>ISPrimary</p>
+              <p>ISSecondary</p>
+              <p>PSPlus</p>
+              <p>PSPlusEpiry</p>
+              <p>Show Price</p>
+              <p>ISAvailable</p>
+            </span>
+          </label>
+          <input type="checkbox" id="touch" />
+          <ul class="slide">
+            <li>
+              <a href="#">Game 1</a>
+            </li>
+            <li>
+              <a href="#">Game 2</a>
+            </li>
+            <li>
+              <a href="#">Game 3</a>
+            </li>
+            <li>
+              <a href="#">Game 4</a>
+            </li>
+          </ul>
+        </nav>
+    </>
+  );
+};
 
-export default AllAccount
+export default AllAccount;
